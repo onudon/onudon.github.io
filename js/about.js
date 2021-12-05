08d1097bf09d9e4546624339da5c21c6bc61a4cf
onudon <89720624+onudon@users.noreply.github.com>
2021-12-05 12:19:31 +0900

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -12,7 +12,26 @@ const windowtick = () => {
 	}
 	setTimeout(windowtick,100);
 }
+let scroll = 0;
 function start(){
 	windowtick();
 	$('#alls').show();
+	$('body').css('overflow-y','scroll')
+	$(window).scroll(function (){
+		if ($(this).scrollTop() == 0 && scroll >= 0) {
+			scroll -= 1.2;
+		}else{
+			scroll += 1.2;
+		}
+		let top = 100 - scroll;
+		let opacity = scroll / 100;
+		window.scrollTo(0,1);
+		$('#about').css('top',top + 'vh');
+		if (opacity <= 0.8) {
+			$('#opacity').css('opacity',opacity);
+		}
+	})
 }
+$('#bottom').click(function(){
+	alert("Pls scroll.")
+})
