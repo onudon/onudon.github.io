ca659378ae6d72b33913897b737da00bc1c8ab04
onudon <89720624+onudon@users.noreply.github.com>
2021-12-09 21:12:07 +0900

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -2,45 +2,30 @@ $(function(){
 	setTimeout(function(){$('#anime').hide();}, 4000)
 	setTimeout(function(){start()},4500)
 })
-var check = true;
 const windowtick = () => {
 	let height = window.innerHeight;
 	let width = window.innerWidth;
 	if(width > height) {
-		check = true;
 		$('.title').css({width:'60%',left:'20%'});
 		$('#opacity').css('height','100%');
 	}else {
-		check = false;
 		$('.title').css({width:'100%',left:'0%'});
 		$('#opacity').css('height','0%');
 		$('#about').css('top','65%');
 	}
 	setTimeout(windowtick,100);
 }
-let scroll = 0;
 function start(){
 	windowtick();
 	$('#alls').show();
 	$('body').css('overflow-y','scroll')
 	$(window).scroll(function (){
-		if (check) {
-			if ($(this).scrollTop() <= 0) {
-				scroll -= 2;
-			}else if($(this).scrollTop() >= 2) {
-				scroll += 2;
-			}
-			if (scroll < 0) {
-				scroll = 0;
-			}
-			let top = 100 - scroll;
-			let opacity = scroll / 100;
-			console.log(scroll);
-			$('#about').css('top',top + 'vh');
-			if (opacity <= 0.8) {
-				$('#opacity').css('opacity',opacity);
-			}
-			window.scrollTo(0,2);
+		let scroll = $(this).scrollTop() / 10;
+		let top = 100 - scroll;
+		let opacity = scroll / 100;
+		console.log(scroll);
+		if (opacity <= 0.8) {
+			$('#opacity').css('opacity',opacity);
 		}
 	})
 }
